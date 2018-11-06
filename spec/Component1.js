9ec727c79a7b0097b4319a2a84d498ea36db6e55
Dylaann <dyl.mrph@gmail.com>
2018-11-06 10:12:53 +0000

    COM-40 : Test Code Added

---
+++ spec/Component1.js
@@ -0,0 +1,10 @@
+/* global Component1, describe, it, expect, should */
+
+describe('Component1()', function () {
+  'use strict';
+
+  it('MyImage Class', function () {
+    expect(MyImage).to.be.a('function');
+  });
+  
+});
